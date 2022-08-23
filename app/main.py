be3abd247b41016bc3d07a2b3d7133876234a022
Agnieszka Kasztalska <agnieszkakasztalska@MacBook-Pro-Agnieszka-K.local>
2022-08-23 18:59:28 +0200

    refactoring main - adding repository

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, APIRouter, Depends, HTTPException
+from fastapi import FastAPI, APIRouter, Depends
 from sqlalchemy.orm import Session
 
 
@@ -6,9 +6,11 @@ import app.models as models
 import app.schemas as schemas
 
 from app.database import Base, engine, SessionLocal
+from app.repository import Repository
 
 # creates database
 Base.metadata.create_all(engine)
+repository = Repository()
 
 
 def get_session():
@@ -25,34 +27,27 @@ api_router = APIRouter()
 
 @api_router.get("/")
 def get_all_books(session: Session = Depends(get_session)):
-    books = session.query(models.Book).all()
+    books = repository.get_all(models.Book, session)
     return books
 
 
 @api_router.post("/")
 def add_book(book: schemas.Book, session: Session = Depends(get_session)):
     book = models.Book(title=book.title, author=book.author, price=book.price)
-    session.add(book)
-    session.commit()
-    session.refresh(book)
+    repository.add_object(session, book)
     return book
 
 
 @api_router.get("/{book_id}")
 def get_book(book_id: int, session: Session = Depends(get_session)):
-    book = session.query(models.Book).get(book_id)
-    if not book:
-        raise HTTPException(status_code=404, detail=f"Book with ID {book_id} not found")
-    return book
+    return repository.get_one(book_id, session, models.Book)
 
 
 @api_router.put("/{book_id}")
 def update_book(
     book_id: int, book: schemas.Book, session: Session = Depends(get_session)
 ):
-    book_updated = session.query(models.Book).get(book_id)
-    if not book_updated:
-        raise HTTPException(status_code=404, detail=f"Book with ID {book_id} not found")
+    book_updated = repository.get_one(book_id, session)
     book_updated.title = book.title
     book_updated.author = book.author
     book_updated.price = book.price
@@ -62,13 +57,7 @@ def update_book(
 
 @api_router.delete("/{book_id}")
 def delete_book(book_id: int, session: Session = Depends(get_session)):
-    book_item = session.query(models.Book).get(book_id)
-    if not book_item:
-        raise HTTPException(status_code=404, detail=f"Book with ID {book_id} not found")
-    session.delete(book_item)
-    session.commit()
-    session.close()
-    return "Book was deleted!"
+    return repository.delete_object(book_id, models.Book, session)
 
 
 app.include_router(api_router)
---
+++ app/repository.py
@@ -0,0 +1,27 @@
+from fastapi import HTTPException
+
+
+class Repository:
+    def get_all(self, model, session):
+        docs = session.query(model).all()
+        return docs
+
+    def get_one(self, object_id, session, model):
+        object = session.query(model).get(object_id)
+        if not object:
+            raise HTTPException(
+                status_code=404, detail=f"Object with ID {object_id} not found"
+            )
+        return object
+
+    def add_object(self, session, object):
+        session.add(object)
+        session.commit()
+        session.refresh(object)
+
+    def delete_object(self, object_id, model, session):
+        object = self.get_one(object_id, session, model)
+        session.delete(object)
+        session.commit()
+        session.close()
+        return "Object was deleted!"
