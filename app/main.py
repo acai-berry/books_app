5245be9c82bdb03a7202d9fa2ecca330b60eea07
Agnieszka Kasztalska <agnieszkakasztalska@MacBook-Pro-Agnieszka-K.local>
2022-09-02 15:06:39 +0200

    refactored exception handling

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI, APIRouter, HTTPException, status
 from http import HTTPStatus
 
 import app.models as models
-from app.repository import Err, NO_OBJECT_ERROR, SQLiteRepository
+from app.repository import SQLiteRepository, NotFoundError
 from app import controller
 
 
@@ -13,11 +13,6 @@ api_router = APIRouter()
 @api_router.get("/books/", tags=["books"], response_model=list[models.Book])
 async def get_all_books():
     response = await controller.fetch_all_books(SQLiteRepository)
-    if type(response) == Err:
-        raise HTTPException(
-            status_code=HTTPStatus.INTERNAL_SERVER_ERROR.value,
-            detail="Server error, cannot handle the request.",
-        )
     return response
 
 
@@ -29,40 +24,15 @@ async def get_all_books():
 )
 async def add_book(book: models.BookIn):
     response = await controller.add_book(SQLiteRepository, book)
-    if type(response) == Err:
-        raise HTTPException(
-            status_code=HTTPStatus.INTERNAL_SERVER_ERROR.value,
-            detail="Server error, cannot handle the request.",
-        )
     return response
 
 
 @api_router.get("/books/{book_id}", tags=["books"], response_model=models.Book)
 async def get_book(book_id: int):
     response = await controller.fetch_a_book(SQLiteRepository, book_id)
-    if type(response) == Err and response.error == NO_OBJECT_ERROR:
+    if isinstance(response, NotFoundError):
         raise HTTPException(
             status_code=HTTPStatus.NOT_FOUND.value, detail="Book with such ID not found"
-        )
-    elif type(response) == Err:
-        raise HTTPException(
-            status_code=HTTPStatus.INTERNAL_SERVER_ERROR.value,
-            detail="Server error, cannot handle the request.",
-        )
-    return response
-
-
-@api_router.put("/books/{book_id}", tags=["books"], response_model=models.Book)
-async def update_book(book_id: int, book: models.BookIn):
-    response = await controller.update_book(SQLiteRepository, book_id, book)
-    if type(response) == Err and response.error == NO_OBJECT_ERROR:
-        raise HTTPException(
-            status_code=HTTPStatus.NOT_FOUND.value, detail="Book with such ID not found"
-        )
-    elif type(response) == Err:
-        raise HTTPException(
-            status_code=HTTPStatus.INTERNAL_SERVER_ERROR.value,
-            detail="Server error, cannot handle the request.",
         )
     return response
 
@@ -70,14 +40,19 @@ async def update_book(book_id: int, book: models.BookIn):
 @api_router.delete("/books/{book_id}", tags=["books"])
 async def delete_book(book_id: int):
     response = await controller.delete_book(SQLiteRepository, book_id)
-    if type(response) == Err and response.error == NO_OBJECT_ERROR:
+    if response == 0:
         raise HTTPException(
             status_code=HTTPStatus.NOT_FOUND.value, detail="Book with such ID not found"
         )
-    elif type(response) == Err:
+    return response
+
+
+@api_router.put("/books/{book_id}", tags=["books"], response_model=models.Book)
+async def update_book(book_id: int, book: models.BookIn):
+    response = await controller.update_book(SQLiteRepository, book_id, book)
+    if isinstance(response, NotFoundError):
         raise HTTPException(
-            status_code=HTTPStatus.INTERNAL_SERVER_ERROR.value,
-            detail="Server error, cannot handle the request.",
+            status_code=HTTPStatus.NOT_FOUND.value, detail="Book with such ID not found"
         )
     return response
 
--- app/repository.py
+++ app/repository.py
@@ -1,71 +1,43 @@
-from sqlalchemy.exc import SQLAlchemyError
-from dataclasses import dataclass
 from app.database import database
 
 
-@dataclass
-class Err:
-    error: str
-
-
-NO_OBJECT_ERROR = "No such object!"
+class NotFoundError(Exception):
+    pass
 
 
 class SQLiteRepository:
     @staticmethod
     async def fetch_all(table):
-        try:
-            query = table.select()
-            return await database.fetch_all(query)
-        except SQLAlchemyError as err:
-            return Err(str(err))
+        query = table.select()
+        return await database.fetch_all(query)
 
     @staticmethod
     async def fetch_one(table, object_id):
-        try:
-            query = table.select().where(table.c.id == object_id)
-            data = await database.fetch_one(query)
-            if not data:
-                return Err(NO_OBJECT_ERROR)
-            else:
-                return data
-        except SQLAlchemyError as err:
-            return Err(str(err))
+        query = table.select().where(table.c.id == object_id)
+        data = await database.fetch_one(query)
+        if not data:
+            return NotFoundError("No such object.")
+        return data
 
     @staticmethod
     async def add_values(table, values):
-        try:
-            query = table.insert().values(values)
-            record_id = await database.execute(query)
-            query = table.select().where(table.c.id == record_id)
-            data = await database.fetch_one(query)
-            return data
-        except SQLAlchemyError as err:
-            return Err(str(err))
-
-    @staticmethod
-    async def update_values(table, updated_values, object_id):
-        try:
-            query = table.update().where(table.c.id == object_id).values(updated_values)
-            await database.execute(query)
-            query = table.select().where(table.c.id == object_id)
-            data = await database.fetch_one(query)
-            if not data:
-                return Err(NO_OBJECT_ERROR)
-            else:
-                return data
-        except SQLAlchemyError as err:
-            return Err(str(err))
+        query = table.insert().values(values)
+        record_id = await database.execute(query)
+        query = table.select().where(table.c.id == record_id)
+        data = await database.fetch_one(query)
+        return data
 
     @staticmethod
     async def delete_object(table, object_id):
-        try:
-            query = table.select().where(table.c.id == object_id)
-            data = await database.fetch_one(query)
-            if not data:
-                return Err(NO_OBJECT_ERROR)
-            else:
-                query = table.delete().where(table.c.id == object_id)
-                return await database.execute(query)
-        except SQLAlchemyError as err:
-            return Err(str(err))
+        query = table.delete().where(table.c.id == object_id)
+        return await database.execute(query)
+
+    @staticmethod
+    async def update_values(table, updated_values, object_id):
+        query = table.update().where(table.c.id == object_id).values(updated_values)
+        await database.execute(query)
+        query = table.select().where(table.c.id == object_id)
+        data = await database.fetch_one(query)
+        if not data:
+            return NotFoundError("No such object.")
+        return data
