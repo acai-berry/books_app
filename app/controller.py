a0780e39e1f6d1a3ee8da608c9fbda1e6c4c90c6
Agnieszka Kasztalska <agnieszkakasztalska@MacBook-Pro-Agnieszka-K.local>
2022-09-02 13:11:52 +0200

    adding dependency injection to controller

--- app/controller.py
+++ app/controller.py
@@ -1,24 +1,23 @@
-from app.repository import SQLiteRepository
 from app.database import books
 
 
-async def fetch_all_books():
-    return await SQLiteRepository.fetch_all(books)
+async def fetch_all_books(repository):
+    return await repository.fetch_all(books)
 
 
-async def add_book(book):
+async def add_book(repository, book):
     values = {"title": book.title, "author": book.author, "price": book.price}
-    return await SQLiteRepository.add_values(books, values)
+    return await repository.add_values(books, values)
 
 
-async def fetch_a_book(book_id):
-    return await SQLiteRepository.fetch_one(books, book_id)
+async def fetch_a_book(repository, book_id):
+    return await repository.fetch_one(books, book_id)
 
 
-async def update_book(book_id, book):
+async def update_book(repository, book_id, book):
     updated_values = {"title": book.title, "author": book.author, "price": book.price}
-    return await SQLiteRepository.update_values(books, updated_values, book_id)
+    return await repository.update_values(books, updated_values, book_id)
 
 
-async def delete_book(book_id):
-    return await SQLiteRepository.delete_object(books, book_id)
+async def delete_book(repository, book_id):
+    return await repository.delete_object(books, book_id)
--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI, APIRouter, HTTPException, status
 from http import HTTPStatus
 
 import app.models as models
-from app.repository import Err, NO_OBJECT_ERROR
+from app.repository import Err, NO_OBJECT_ERROR, SQLiteRepository
 from app import controller
 
 
@@ -12,7 +12,7 @@ api_router = APIRouter()
 
 @api_router.get("/books/", tags=["books"], response_model=list[models.Book])
 async def get_all_books():
-    response = await controller.fetch_all_books()
+    response = await controller.fetch_all_books(SQLiteRepository)
     if type(response) == Err:
         raise HTTPException(
             status_code=HTTPStatus.INTERNAL_SERVER_ERROR.value,
@@ -28,7 +28,7 @@ async def get_all_books():
     status_code=status.HTTP_201_CREATED,
 )
 async def add_book(book: models.BookIn):
-    response = await controller.add_book(book)
+    response = await controller.add_book(SQLiteRepository, book)
     if type(response) == Err:
         raise HTTPException(
             status_code=HTTPStatus.INTERNAL_SERVER_ERROR.value,
@@ -39,7 +39,7 @@ async def add_book(book: models.BookIn):
 
 @api_router.get("/books/{book_id}", tags=["books"], response_model=models.Book)
 async def get_book(book_id: int):
-    response = await controller.fetch_a_book(book_id)
+    response = await controller.fetch_a_book(SQLiteRepository, book_id)
     if type(response) == Err and response.error == NO_OBJECT_ERROR:
         raise HTTPException(
             status_code=HTTPStatus.NOT_FOUND.value, detail="Book with such ID not found"
@@ -54,7 +54,7 @@ async def get_book(book_id: int):
 
 @api_router.put("/books/{book_id}", tags=["books"], response_model=models.Book)
 async def update_book(book_id: int, book: models.BookIn):
-    response = await controller.update_book(book_id, book)
+    response = await controller.update_book(SQLiteRepository, book_id, book)
     if type(response) == Err and response.error == NO_OBJECT_ERROR:
         raise HTTPException(
             status_code=HTTPStatus.NOT_FOUND.value, detail="Book with such ID not found"
@@ -69,7 +69,7 @@ async def update_book(book_id: int, book: models.BookIn):
 
 @api_router.delete("/books/{book_id}", tags=["books"])
 async def delete_book(book_id: int):
-    response = await controller.delete_book(book_id)
+    response = await controller.delete_book(SQLiteRepository, book_id)
     if type(response) == Err and response.error == NO_OBJECT_ERROR:
         raise HTTPException(
             status_code=HTTPStatus.NOT_FOUND.value, detail="Book with such ID not found"
