881b9613d1692373434f256485c414cea6e47806
Agnieszka Kasztalska <agnieszkakasztalska@MacBook-Pro-Agnieszka-K.local>
2022-09-06 16:16:39 +0200

    tests refatcored - fixtures added

--- app/tests/test_app.py
+++ app/tests/test_app.py
@@ -11,20 +11,7 @@ def test_health(test_client):
     assert response.json() == {"health": "It's working ✨"}
 
 
-def test_should_create_a_book(test_client):
-    # given
-    test_request_payload = {
-        "title": "something",
-        "author": "something else",
-        "price": 10.0,
-    }
-    test_response_payload = {
-        "id": 1,
-        "title": "something",
-        "author": "something else",
-        "price": 10.0,
-    }
-
+def test_should_create_a_book(test_client, test_request_payload, test_response_payload):
     # when
     response = test_client.post(
         main.app.url_path_for("add_book"),
@@ -35,19 +22,12 @@ def test_should_create_a_book(test_client):
     assert response.json() == test_response_payload
 
 
-def test_should_get_a_book(test_client):
-    # given
-    test_data = {
-        "id": 1,
-        "title": "something",
-        "author": "something else",
-        "price": 10.0,
-    }
+def test_should_get_a_book(test_client, test_response_payload):
     # when
     response = test_client.get(main.app.url_path_for("get_book", book_id=1))
     # then
     assert response.status_code == HTTPStatus.OK.value
-    assert response.json() == test_data
+    assert response.json() == test_response_payload
 
 
 def test_should_get_404_error_no_such_book(test_client):
@@ -67,28 +47,17 @@ def test_should_get_all_books(test_client):
     assert len(data) == 1
 
 
-def test_should_update_a_book_entry(test_client):
-    # given
-    test_update_data = {
-        "title": "test_title1_updated",
-        "author": "test_author1",
-        "price": 9.99,
-    }
-    expected_data = {
-        "id": 1,
-        "title": "test_title1_updated",
-        "author": "test_author1",
-        "price": 9.99,
-    }
-
+def test_should_update_a_book_entry(
+    test_client, test_update_request_data, test_update_response_data
+):
     # when
     response = test_client.put(
         main.app.url_path_for("update_book", book_id=1),
-        data=json.dumps(test_update_data),
+        data=json.dumps(test_update_request_data),
     )
     # then
     assert response.status_code == HTTPStatus.OK.value
-    assert response.json() == expected_data
+    assert response.json() == test_update_response_data
 
 
 def test_should_successfully_delete_a_book(test_client):
